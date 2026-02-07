8b6082cb9dcc627f1bc4bea9f1825d75e4380f25
yuta <iiitoobee@gmail.com>
2026-02-07 17:29:06 +0300

    database

---
+++ main/shop/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug' : ('name',) }
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available', 'createddate', 'updated']
+    list_filter = ['available', 'createddate', 'updated', 'category']
+    list_editable  = ['price', 'available']
+    prepopulated_fields = {'slug' : ('name', ) } 
